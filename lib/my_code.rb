44cb15ab3b249bf58a0b5de64a30f0e8a403f0fd
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 17:34:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,6 +11,20 @@ def map(array)
 end
 
 
-def reduce(array, sv=0)
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0] 
+    i = 1
+  end
 
+  while i < array.length
+    yield(sum, array[i])
+
+    i += 1
+  end
+  sum
+    
 end
