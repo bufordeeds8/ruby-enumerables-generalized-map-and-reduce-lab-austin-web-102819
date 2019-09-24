0349ca4d968265b728da19fa8e29fcbac2c38cc9
Buford Eeds <bufordeeds8@gmail.com>
2019-09-24 17:19:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(array)
+  # map returns a new array of the manipulated elements
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+
+def reduce(array, sv=nil)
+
+end
